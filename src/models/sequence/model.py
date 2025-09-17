f44061912843ad6d6e64744a1b7cc9b55b03a7d4
Apuroop Mutyala <apumutyala@gmail.com>
2025-09-17 12:06:26 -0400

    trying different absolute imports now

--- src/models/sequence/model.py
+++ src/models/sequence/model.py
@@ -15,10 +15,15 @@ from einops import rearrange
 # from src.models.nn.components import Normalization, DropoutNd
 
 # changing to relative imports so we can use submodules outside of this directory
-from ...utils.config import to_list, to_dict
-from .block import SequenceResidualBlock
-from .base import SequenceModule  
-from ..nn.components import Normalization, DropoutNd
+# from ...utils.config import to_list, to_dict
+# from .block import SequenceResidualBlock
+# from .base import SequenceModule  
+# from ..nn.components import Normalization, DropoutNd
+
+from utils.config import to_list, to_dict
+from models.sequence.block import SequenceResidualBlock
+from models.sequence.base import SequenceModule  
+from models.nn.components import Normalization, DropoutNd
 
 
 class SequenceModel(SequenceModule):
